1e1cd5712c219959d30f32b818873b7faff62d9b
Daniil <80905799+DaniilGavrin@users.noreply.github.com>
2024-11-18 14:46:07 +0300

    update 10

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
-import 'screens/project_screen.dart';
-import 'screens/task_screen.dart';
+import '/screens/home_screen.dart';
+import '/screens/project_screen.dart';
+import '/screens/task_screen.dart';
 
 void main() {
-  runApp(TaskManagerApp());
+  runApp(const MyApp());
 }
 
-class TaskManagerApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Менеджер задач',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      initialRoute: '/',
+      title: 'Task Manager',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/home',
       routes: {
-        '/': (context) => HomeScreen(),
-        '/project': (context) => ProjectScreen(),
-        '/task': (context) => TaskScreen(),
+        '/home': (context) => const HomeScreen(),
+        '/projects': (context) => ProjectScreen(),
+        '/tasks': (context) => TaskScreen(),
       },
     );
   }
